8ba4f68cbc87b59ab28d62bcb12e69ef6fb9711b
Sri Panyam <sri.panyam@gmail.com>
2025-11-23 21:13:07 -0800

    Adding imports from lib

---
+++ services/imports.go
@@ -0,0 +1,16 @@
+package services
+
+import "github.com/turnforge/weewar/lib"
+
+type AxialCoord = lib.AxialCoord
+type Position = lib.Position
+type NeighborDirection = lib.NeighborDirection
+
+var CoordFromInt32 = lib.CoordFromInt32
+var CubeDistance = lib.CubeDistance
+var GetDirection = lib.GetDirection
+var DirectionToString = lib.DirectionToString
+var DirectionToLongString = lib.DirectionToLongString
+var RowColToHex = lib.RowColToHex
+var HexToRowCol = lib.HexToRowCol
+var ParseDirection = lib.ParseDirection
